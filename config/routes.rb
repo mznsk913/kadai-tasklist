a1079ba405e67e3bd54a33b53114acb885a0b32b
mznsk913 <mizunosaki9@gmail.com>
2023-11-05 07:41:53 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root to: 'tasks#index' 
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
   resources :tasks
+  
+#  get 'users/index'
+#  get 'users/show'
+#  get 'users/new'
+#  get 'users/create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_04_054911) do
+ActiveRecord::Schema.define(version: 2023_11_05_044158) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2023_11_04_054911) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.string "status"
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
